b4201116fcca4d067c307d4473b7f2d2868297e1
Bjarke Gotfredsen <bjarke@gotfredsen.com>
2022-03-31 10:39:39 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h>
 
-void setup() {
-Serial.begin(115200);
-}
+void setup() { Serial.begin(115200); }
 
 void loop() {
-Serial.println("Hello");
-delay(1500);
+  Serial.println("Hello");
+  delay(1500);
 }
